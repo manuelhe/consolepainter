f849837b54100c6824cbabb94a3887c7be5f1dd0
Manuel Herrera <manuel.he@gmail.com>
2015-02-08 22:01:00 -0500

    Fixed PSR-4 autoloading

--- app/console.php
+++ app/console.php
@@ -1,1 +1,14 @@
 #!/usr/bin/env php
+<?php
+
+// set to run indefinitely if needed
+set_time_limit(0);
+
+require __DIR__ . '/vendor/autoload.php';
+
+use ManuelHe\Command\ConsolePainter;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+$application->add(new ConsolePainter);
+$application->run();
